76d6a8cf1b844ce48c1fde8cfcfa67accedd707d
iamsojung <38772161+iamsojung@users.noreply.github.com>
2021-11-13 18:46:10 +0900

    Time: 9 ms (32.70%), Space: 44.4 MB (69.87%) - LeetHub

--- majority-element/majority-element.java
+++ majority-element/majority-element.java
@@ -3,38 +3,33 @@ class Solution {
         
         HashMap<Integer, Integer> hm = new HashMap<>();
         int max = 0;
-
-        for(int i=0;i<nums.length;i++){
-            if(hm.containsKey(nums[i])){
-                int cnt = hm.get(nums[i]);
-                
-                    hm.put(nums[i],cnt+1);
-                    max = Math.max(cnt+1,max);
-                } else{
-            
-                    hm.put(nums[i],1);
-                max = Math.max(1,max);
-               
-            }
-
-        }
-        int answer = 0;
-        System.out.println(nums.length/2+1);
         
+        for(int i=0;i<nums.length;i++){
+            
+            if(hm.containsKey(nums[i])){
+                int num = hm.get(nums[i]);
+                
+                hm.put(nums[i],num+1);
+                max = Math.max(max,num+1);
+            }else{
+                
+            hm.put(nums[i],1);
+                max = Math.max(max,1);
+            }
+        }
         Iterator<Integer> keys = hm.keySet().iterator();
         while( keys.hasNext() ){
             int key = keys.next();
-            System.out.println( key+""+ hm.get(key) );
-            
+      //      System.out.println( String.format("키 : %s, 값 : %s", key, map.get(key)) );
             if(hm.get(key)==max){
-                if(hm.get(key)>=(nums.length/2)){
-                    
-                    answer = key;
-                    System.out.println("zzzzz"+answer);
-                }
-                
+                return key;
             }
-        }        
-        return answer;
+        }
+        
+        return 0;
+
     }
 }
+
+        
+ 
