94f886bbbc2303099c83937c2114ecad264d7330
iamsojung <38772161+iamsojung@users.noreply.github.com>
2023-02-13 22:07:30 +0900

    Time: 5 ms (92.75%), Space: 42.4 MB (59.34%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.java
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -2,10 +2,14 @@ class Solution {
     public String getHint(String secret, String guess) {
         int bulls=0;
         int cows=0;
+        
         int[] nums=new int[10];
+        
         for(int i=0;i<secret.length();i++){
+            
             int numsSecret=secret.charAt(i)-'0';
             int numsGuess=guess.charAt(i)-'0';
+            
             if(numsSecret==numsGuess){
                 bulls++;
             }else{
