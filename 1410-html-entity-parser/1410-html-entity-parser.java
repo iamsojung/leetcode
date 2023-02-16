67619adefe6df9f4f9c8f6c70060a1256b83e530
iamsojung <38772161+iamsojung@users.noreply.github.com>
2023-02-16 22:06:56 +0900

    Time: 41 ms (48.86%), Space: 42.9 MB (81.82%) - LeetHub

---
+++ 1410-html-entity-parser/1410-html-entity-parser.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String entityParser(String text) {
+        return text.replaceAll("&quot;", "\"")
+                .replaceAll("&apos;", "'")
+                .replaceAll("&gt;", ">")
+                .replaceAll("&lt;", "<")
+                .replaceAll("&frasl;", "/")
+                .replaceAll("&amp;", "&");
+    }
+}
