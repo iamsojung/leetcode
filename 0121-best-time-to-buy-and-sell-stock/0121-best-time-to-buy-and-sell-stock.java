c019a720f63066f98f7dc358435e76c585e43024
iamsojung <38772161+iamsojung@users.noreply.github.com>
2023-02-27 22:10:15 +0900

    Time: 2 ms (67.58%), Space: 59 MB (56.31%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,6 +1,6 @@
 class Solution {
    public int maxProfit(int[] prices) {
-    int max =0;
+    int max = 0;
     int min = Integer.MAX_VALUE;
     
     for(int i =0; i<prices.length;i++){
@@ -12,6 +12,6 @@ class Solution {
         }
         
     }
-    return max;
+        return max;
     }
 }
