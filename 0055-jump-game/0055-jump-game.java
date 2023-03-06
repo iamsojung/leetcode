bd16682fdc004b9bd5e4478d7c630a134e257a74
iamsojung <38772161+iamsojung@users.noreply.github.com>
2023-03-06 21:33:18 +0900

    Time: 1 ms (99.99%), Space: 42.7 MB (69.82%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
         if (nums == null || nums.length == 0) return true;
-        int n = nums.length, lastPos = n - 1;
-        for (int i = lastPos; i >= 0; i--) { 
-            if (i + nums[i] >= lastPos) lastPos = i;
+        int n = nums.length;
+        int lastPos = n - 1;
+        for (int i = lastPos; i >= 0; i--) {  //가장 마지막부터 0이 될 때까지
+            if (i + nums[i] >= lastPos) lastPos = i; //i와 배열의 숫자가 lastPose보다 같거나 크다면, lastPos를 i로 변경,,
         }
         return lastPos == 0;
     }
