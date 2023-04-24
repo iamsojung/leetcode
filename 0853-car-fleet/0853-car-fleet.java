b783af9051cddd17c40e5cb12553f25ec4eef7c8
iamsojung <38772161+iamsojung@users.noreply.github.com>
2023-04-24 20:19:43 +0900

    Time: 107 ms (68.31%), Space: 56.7 MB (45.69%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    class Car {
+        int position;
+        int speed;
+        public Car(int position, int speed) {
+            this.position = position;
+            this.speed = speed;
+        }
+    }
+    
+    public int carFleet(int target, int[] position, int[] speed) {
+        Car[] cars = new Car[position.length];
+        for (int i = 0; i < position.length; i++) {
+            cars[i] = new Car(position[i], speed[i]);
+        }
+        
+        // sort cars in ascending order by position
+        Arrays.sort(cars, (a, b) -> a.position - b.position);
+        
+        Stack<Double> fleets = new Stack<>();
+        for (int i = cars.length - 1; i >= 0; i--) {
+            // 이 차가 목표에 도달하기까지 몇 초가 걸리는지 계산한다.
+            double time = (double) (target - cars[i].position) / cars[i].speed;
+            // check if this car will get stuck behind a car that was ahead
+            if (!fleets.isEmpty() && time <= fleets.peek()) {
+                // 뒤에 있는 더 빠른 차량이 앞으로 못가게 하기 위해서 더 느린 자동차의 시간을 스택의 맨 위에 유지한다.
+                continue;
+            }
+            fleets.push(time);
+        }
+        
+        return fleets.size();
+    }
+}
