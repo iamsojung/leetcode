f050ed1fc1317a6641399f8c2e8b7d1d01a6cd16
iamsojung <38772161+iamsojung@users.noreply.github.com>
2023-04-24 20:23:37 +0900

    Time: 107 ms (68.31%), Space: 56.9 MB (40.83%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -22,7 +22,7 @@ class Solution {
         for (int i = cars.length - 1; i >= 0; i--) {
             // 이 차가 목표에 도달하기까지 몇 초가 걸리는지 계산한다.
             double time = (double) (target - cars[i].position) / cars[i].speed;
-            // check if this car will get stuck behind a car that was ahead
+            // 앞에 있는 차한테 갇히는지 확인한다
             if (!fleets.isEmpty() && time <= fleets.peek()) {
                 // 뒤에 있는 더 빠른 차량이 앞으로 못가게 하기 위해서 더 느린 자동차의 시간을 스택의 맨 위에 유지한다.
                 continue;
