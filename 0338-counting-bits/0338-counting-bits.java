0110e60f3d70edbd4460dcf28b555610e45d025a
iamsojung <38772161+iamsojung@users.noreply.github.com>
2025-06-24 20:38:20 +0900

    Time: 2 ms (94.47%), Space: 46.6 MB (83.08%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,18 +1,10 @@
 class Solution {
-    public int[] countBits(int n) {
-        int [] arr = new int[n+1];
-        for(int i=0;i<=n;i++){
-            arr[i]=Bits(i);
-        }
-        return arr;
-    }
-
-    public static int Bits(int n){
-        int count = 0;
-       while(n!=0){
-            count+=(n&1);
-            n=n>>1;
-       }
-       return count;
+    public int[] countBits(int num) {
+        int[] f = new int[num + 1];
+        for (int i=1; i<=num; i++) f[i] = f[i >> 1] + (i & 1);
+        // f[i >> 1]은 i를 오른쪽으로 1비트 시프트한 값의 결과 (i를 2로 나눈 몫)
+        // (i & 1)은 i의 마지막 비트가 1인지 0인지를 판단 (i가 홀수면 1, 짝수면 0)
+        // 즉, i의 이진수에서 1의 개수 = i를 2로 나눈 수의 1의 개수 + i가 홀수인지 여부
+        return f;
     }
 }
